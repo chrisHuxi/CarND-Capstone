dbd9320be3e22d5e65bf2867c02d617d0d68d011
chrisHuxi <chris_huxi@163.com>
2019-07-23 13:34:39 +0200

    fixed bugs in dbw_node

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -7,7 +7,7 @@ class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
         # TODO: 
-        #pass
+        pass
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
